3518369f71aab93fa3a1918064e85dff012edf5c
Siddharth Bhosale <Siddharth.17u198@viit.ac.in>
2020-10-07 17:31:15 +0530

    adding files

---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable eol-last */
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function HomeScreen(props) {
+    return (
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    );
+}
---
+++ src/screens/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+
+
+/* eslint-disable prettier/prettier */
+
+export { default as LoginScreen } from './LoginScreen/LoginScreen';
+
+export { default as HomeScreen } from './HomeScreen/HomeScreen';
+
+
+export { default as RegistrationScreen } from './RegistrationScreen/RegistrationScreen';
